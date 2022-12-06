fbd82caa89ff624a8549f544bf289e2459161ba0
DongYeon Kang <myjjue00@gmail.com>
2022-12-06 13:22:49 +0900

    feat : Join 구현하기

---
+++ src/main/java/com/example/springbootsecurity/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.springbootsecurity.controller;
+
+import com.example.springbootsecurity.domain.dto.UserJoinRequest;
+import com.example.springbootsecurity.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@RequestBody UserJoinRequest dto) {
+        return ResponseEntity.ok().body("회원가입 성공");
+    }
+
+}
---
+++ src/main/java/com/example/springbootsecurity/service/UserService.java
@@ -0,0 +1,28 @@
+package com.example.springbootsecurity.service;
+
+import com.example.springbootsecurity.domain.User;
+import com.example.springbootsecurity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public String join(String userName, String password) {
+        // userName 중복 체크
+        userRepository.findByUserName(userName)
+                .ifPresent(user -> {
+                    throw new RuntimeException(userName + "은 이미 있습니다.");
+                });
+        // 저장
+        User user = User.builder()
+                .userName(userName)
+                .password(password)
+                .build();
+        userRepository.save(user);
+
+        return "SUCCESS";
+    }
+}
